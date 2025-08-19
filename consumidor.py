7159508e854f95b3c0c5fbb4bf85e26ac3d1b43a
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-08-18 22:08:20 -0300

    feat: Fila de teste ok

---
+++ consumidor.py
@@ -0,0 +1,62 @@
+from typing import Tuple
+import pika
+from pika.adapters.blocking_connection import BlockingChannel
+from src.config.config import Config
+from src.conexao_api.i_bikes_api import IBikesAPI
+from src.conexao_api.bike_api import BikesApi
+
+
+class Consumidor:
+
+    def __init__(self, api_bike: IBikesAPI):
+        self.__credenciais = pika.PlainCredentials(
+            Config.USR_RABBITMQ,
+            Config.PWD_RABBITMQ
+        )
+        self.__parametros_conexao = pika.ConnectionParameters(
+            host=Config.HOST_RABBITMQ,
+            port=Config.PORTA_RABBITMQ,
+            virtual_host='/',
+            credentials=self.__credenciais
+        )
+        self.__conexao = pika.BlockingConnection(parameters=self.__parametros_conexao)
+        self.__api_bike = api_bike
+
+    def mostrar_mensagem(
+            self,
+            ch: BlockingChannel,
+            method: pika.spec.Basic.Deliver,
+            properties: pika.spec.BasicProperties,
+            body: bytes
+    ):
+        print('=' * 20)
+        print(body)
+        print('=' * 20)
+
+    def configurar_fila(self, canal: BlockingChannel) -> Tuple[BlockingChannel, str]:
+        canal.exchange_declare(
+            exchange='bicicleta_curitiba',
+            exchange_type='fanout'
+        )
+        result = canal.queue_declare(queue='', exclusive=True)
+        nome_fila = result.method.queue
+        canal.queue_bind(exchange='bicicleta_curitiba', queue=nome_fila)
+        return canal, nome_fila
+
+    def executar(self):
+        canal = self.__conexao.channel()
+        canal, nome_fila = self.configurar_fila(canal=canal)
+        canal.basic_consume(
+            queue=nome_fila,
+            on_message_callback=self.mostrar_mensagem,
+            auto_ack=True
+
+        )
+        canal.start_consuming()
+
+
+if __name__ == '__main__':
+    c = Consumidor(
+        api_bike=BikesApi()
+    )
+    c.executar()
--- produtor.py
+++ produtor.py
@@ -1,2 +1,53 @@
+import time
+import pika
+from src.config.config import Config
+from src.conexao_api.i_bikes_api import IBikesAPI
+from src.conexao_api.bike_api import BikesApi
+import json
+
+
 class Produtor:
-    pass
+
+    def __init__(self, api_bike: IBikesAPI):
+        self.__credenciais = pika.PlainCredentials(
+            Config.USR_RABBITMQ,
+            Config.PWD_RABBITMQ
+        )
+        self.__parametros_conexao = pika.ConnectionParameters(
+            host=Config.HOST_RABBITMQ,
+            port=Config.PORTA_RABBITMQ,
+            virtual_host='/',
+            credentials=self.__credenciais
+        )
+        self.__conexao = pika.BlockingConnection(parameters=self.__parametros_conexao)
+        self.__api_bike = api_bike
+
+    def rodar(self):
+        canal = self.__conexao.channel()
+        canal.exchange_declare(
+            exchange='bicicleta_curitiba',
+            exchange_type='fanout'
+        )
+        while True:
+            try:
+
+                message = self.__api_bike.consultar_dados_bicicleta(estacao='bike-curitiba')
+                print(message)
+                canal.basic_publish(
+                    exchange='bicicleta_curitiba',
+                    routing_key='',
+                    body=json.dumps(message)
+                )
+                print('3 segundos')
+                time.sleep(3)
+
+            except KeyboardInterrupt:
+                print('Fechado')
+                self.__conexao.close()
+
+
+if __name__ == "__main__":
+    p = Produtor(
+        api_bike=BikesApi()
+    )
+    p.rodar()
