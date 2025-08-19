2d6260211671325c017f62e49e237ff453908e53
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-08-19 20:29:59 -0300

    feat: Criando a classe de mensageiro discord

---
+++ mensageiro_discord.py
@@ -0,0 +1,10 @@
+from discord_webhook import DiscordEmbed, DiscordWebhook
+
+
+class MensageiroDiscord:
+
+    def __init__(self):
+        self.__webhook = DiscordWebhook(
+            url=,
+            username='DiscordBOt'
+        )
